39090c64ce53265aacd26ff86e03410033f3b321
朱宇宸 <57423167+asd33435220@users.noreply.github.com>
2021-09-22 23:21:18 +0800

    lc725

--- lc725/main.go
+++ lc725/main.go
@@ -8,15 +8,30 @@ type ListNode struct {
 }
 
 func splitListToParts(head *ListNode, k int) []*ListNode {
+	res := make([]*ListNode, k)
 	count := 0
 	countList := head
-	res := make([]*ListNode, 0)
 	for countList != nil {
 		count++
 		countList = countList.Next
 	}
 	number := count / k
 	rest := count % k
+	curr := head
+	for i := 0; i < k && curr != nil; i++ {
+		res[i] = curr
+		size := number
+		if rest > 0 {
+			size++
+		}
+		for j := 0; j < size-1; j++ {
+			curr = curr.Next
+		}
+		next := curr.Next
+		curr.Next = nil
+		curr = next
+		rest--
+	}
 
 	return res
 }
@@ -27,17 +42,30 @@ func main() {
 		Next: &ListNode{
 			Val: 2,
 			Next: &ListNode{
-				Val: 3,
-				Next: &ListNode{
-					Val: 4,
-					Next: &ListNode{
-						Val:  5,
-						Next: nil,
-					},
-				},
+				Val:  3,
+				Next: nil,
+				// Next: &ListNode{
+				// 	Val: 4,
+				// 	Next: &ListNode{
+				// 		Val: 5,
+				// 		Next: &ListNode{
+				// 			Val: 6,
+				// 			Next: &ListNode{
+				// 				Val: 7,
+				// 				Next: &ListNode{
+				// 					Val: 8,
+				// 					Next: &ListNode{
+				// 						Val:  9,
+				// 						Next: nil,
+				// 					},
+				// 				},
+				// 			},
+				// 		},
+				// 	},
+				// },
 			},
 		},
 	}
-	res := splitListToParts(list, 3)
+	res := splitListToParts(list, 2)
 	fmt.Println("res", res)
 }
