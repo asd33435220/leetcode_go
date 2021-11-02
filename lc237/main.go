7c2bd99cd127156b6aa9ae251cace8abd29dc397
zhuyuchen.9903 <zhuyuchen.9903@bytedance.com>
2021-11-02 10:42:16 +0800

    lc237

---
+++ lc237/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteNode(node *ListNode) {
+	node.Val = node.Next.Val
+	node.Next = node.Next.Next
+}
+
+func main() {
+	root := &ListNode{
+		Val: 2,
+		Next: &ListNode{
+			Val: 0,
+			Next: &ListNode{
+				Val: 1,
+				Next: &ListNode{
+					Val:  3,
+					Next: nil,
+				},
+			},
+		},
+	}
+	deleteNode(root)
+	fmt.Println("root", root)
+}
