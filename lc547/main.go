3389acef924aaf244b01b765023ee26914be158d
zhuyuchen.9903 <zhuyuchen.9903@bytedance.com>
2021-11-20 11:48:01 +0800

    lc547

--- lc547/main.go
+++ lc547/main.go
@@ -3,45 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	isConnected := [][]int{[]int{1, 0, 0, 1}, []int{0, 1, 1, 0}, []int{0, 1, 1, 1}, []int{1, 0, 1, 1}}
-	provincesNumber := findCircleNum(isConnected)
+	nums := []int{1, 3, 2, 2, 5, 2, 3, 7}
+	provincesNumber := findLHS(nums)
 	fmt.Println(provincesNumber)
 }
 
-func findCircleNum(isConnected [][]int) int {
-	var bfs func([][]int, []bool, int, int)
-	bfs = func(isConnected [][]int, visited []bool, citiesNumber int, i int) {
-		var queue []int
-		queue = append(queue, i)
-		for len(queue) > 0 {
-			thisCity := queue[len(queue)-1]
-			queue = queue[:len(queue)-1]
-			if visited[thisCity] {
-				continue
-			}
-			visited[thisCity] = true
+func findLHS(nums []int) (ans int) {
+	myMap := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		myMap[nums[i]]++
+	}
 
-			for j := 0; j < citiesNumber; j++ {
-				if isConnected[j][thisCity] == 1 && !visited[j] {
-					queue = append(queue, j)
-				}
-			}
+	for key, value := range myMap {
+		if v := myMap[key+1]; v != 0 && v+value > ans {
+			ans = v + value
 		}
 	}
-	provincesNumber := 0
-	citiesNumber := len(isConnected[0])
-	visited := make([]bool, citiesNumber)
-	for i := 0; i < citiesNumber; i++ {
-		visited[i] = false
-	}
-	for i := 0; i < citiesNumber; i++ {
-		if visited[i] {
-			continue
-		}
-		bfs(isConnected, visited, citiesNumber, i)
-		provincesNumber++
-
-	}
-
-	return provincesNumber
+	return
 }
